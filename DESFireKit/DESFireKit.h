4043c8ae4effd4ec2e484271fdc4fc22cbba7a52
Fabian Thies <git@fabian-thies.de>
2019-09-01 16:21:22 +0200

    Initial Commit

---
+++ DESFireKit/DESFireKit.h
@@ -0,0 +1,19 @@
+//
+//  DESFireKit.h
+//  DESFireKit
+//
+//  Created by Fabian Thies on 01.09.19.
+//  Copyright © 2019 Fabian Thies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DESFireKit.
+FOUNDATION_EXPORT double DESFireKitVersionNumber;
+
+//! Project version string for DESFireKit.
+FOUNDATION_EXPORT const unsigned char DESFireKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DESFireKit/PublicHeader.h>
+
+
